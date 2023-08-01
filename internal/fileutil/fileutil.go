a9fff14031e9c3d70099f050ed8a8d15bc56af89
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-07-31 22:16:14 -0600

    Add methods to create the config file if doesn't exist

--- internal/fileutil/fileutil.go
+++ internal/fileutil/fileutil.go
@@ -1,6 +1,7 @@
 package fileutil
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -9,7 +10,7 @@ import (
 const configFolderName string = ".cli-local-store"
 const configFileName string = "local-store.json"
 
-func existFile(filePath string) bool {
+func fileExists(filePath string) bool {
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
 		return false
 	}
@@ -31,6 +32,29 @@ func getConfigFilePath() (string, error) {
 
 func Init() {
 	configFilePath, _ := getConfigFilePath()
+	configFileDir := filepath.Dir(configFilePath)
+	data := make(map[string]interface{})
 
-	fmt.Println(existFile(configFilePath))
+	if fileExists(configFilePath) {
+		// read the current content in data
+		fmt.Println("Json file already exists")
+	}
+
+	jsonData, err := json.MarshalIndent(data, "", "	")
+	if err != nil {
+		fmt.Println("Error encoding json file: ", err)
+		return
+	}
+
+	if err := os.MkdirAll(configFileDir, 0755); err != nil {
+		fmt.Println("Error creating the folder path")
+		return
+	}
+
+	if err := os.WriteFile(configFilePath, jsonData, 0644); err != nil {
+		fmt.Println("Error writing json file", err)
+		return
+	}
+
+	fmt.Println("Json file created")
 }
